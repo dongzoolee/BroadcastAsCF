eec9fcffcfc4ff16c5d1f80acb683d5479542537
dongzoolee <nant0313@gmail.com>
2021-02-24 15:20:32 +0900

    Update app.js

--- server_side/app.js
+++ server_side/app.js
@@ -2,19 +2,10 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var socketio = require('socket.io');
-var https = require('https');
-try {
-    var option = {
-        cert: fs.readFileSync('fullchain.pem'),
-        key: fs.readFileSync('privkey.pem')
-    }
-    var server = https.createServer(option, app).listen(3335, () => {
-        console.log('server has started on port 3335');
-    });
-} catch (err) {
-    console.log('an error has occurred');
-    console.warn(err);
-}
+
+var server = app.listen(3335, () => {
+    console.log('server has started on port 3335');
+});
 
 var io = socketio();
 io.attach(server);
@@ -22,9 +13,9 @@ io.on('connection', (socket) => {
 
 });
 
-app.use('/get', (req, res)=>{
+app.use('/get', (req, res) => {
     io.emit('msg', {
         msg: req.query.msg,
-        type:'script'
+        type: 'script'
     })
 })
